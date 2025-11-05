9bc2d6cdf5da1fac23ecf60491aab7c15e71bbb5
RahulV17 <vrahulece@gmail.com>
2025-11-05 20:34:18 +0530

    Updated UserDto, User, and UserRepository classes

--- src/main/java/com/jsp/ebanking/repository/UserRepository.java
+++ src/main/java/com/jsp/ebanking/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.jsp.ebanking.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jsp.ebanking.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }
