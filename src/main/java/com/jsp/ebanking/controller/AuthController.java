0b83b799f6e5c627c6367805f5712d14e265ecbd
RahulV17 <vrahulece@gmail.com>
2025-11-10 19:57:57 +0530

    Controller Config

--- src/main/java/com/jsp/ebanking/controller/AuthController.java
+++ src/main/java/com/jsp/ebanking/controller/AuthController.java
@@ -1,5 +1,51 @@
 package com.jsp.ebanking.controller;
 
+import com.jsp.ebanking.dto.OtpDto;
+import com.jsp.ebanking.dto.ResetPasswordDto;
+import com.jsp.ebanking.dto.ResponseDto;
+import com.jsp.ebanking.dto.UserDto;
+import com.jsp.ebanking.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+	private final UserService userService;
+
+	@PostMapping("/register")
+	public ResponseEntity<ResponseDto> register(@RequestBody @Valid UserDto dto) {
+		return userService.register(dto);
+	}
+
+	@PostMapping("/verify-otp")
+	public ResponseEntity<ResponseDto> verfiyOtp(@RequestBody OtpDto dto) {
+		return userService.verifyOtp(dto);
+	}
+
+	@PatchMapping("/resend-otp/{email}")
+	public ResponseEntity<ResponseDto> resendOtp(@PathVariable String email) {
+		return userService.resendOtp(email);
+	}
+
+	@PatchMapping("/forgot-password/{email}")
+	public ResponseEntity<ResponseDto> forgotPassword(@PathVariable String email) {
+		return userService.forgotPassword(email);
+	}
+
+	@PatchMapping("/reset-password")
+	public ResponseEntity<ResponseDto> resetPassword(@RequestBody @Valid ResetPasswordDto dto) {
+		return userService.resetPassword(dto);
+	}
 
 }
