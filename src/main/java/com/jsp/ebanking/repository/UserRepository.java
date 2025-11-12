77a885e5024321dd1316020b1d1192f32368a651
RahulV17 <vrahulece@gmail.com>
2025-11-12 21:19:22 +0530

    Registration part done

--- src/main/java/com/jsp/ebanking/repository/UserRepository.java
+++ src/main/java/com/jsp/ebanking/repository/UserRepository.java
@@ -6,4 +6,10 @@ import com.jsp.ebanking.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	boolean existsByEmailOrMobile(String email, String mobile);
+	
+	boolean existsByEmail(String email);
+
+	User findByEmail(String email);
+
 }
